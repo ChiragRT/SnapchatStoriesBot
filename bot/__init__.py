abdb12833d80e01b229136dfca06e2e8a68373c2
ChiragRT <154085607+ChiragRT@users.noreply.github.com>
2024-05-16 20:24:21 +0530

    Update __init__.py

--- bot/__init__.py
+++ bot/__init__.py
@@ -10,11 +10,11 @@ logging.getLogger("urllib3").setLevel(logging.WARNING)
 
 
 class ENV_VARS(object):
-    API_ID = int(os.environ.get("API_ID"))
-    API_HASH = os.environ.get("API_HASH")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN")
-    BOT_USERNAME = os.environ.get("BOT_USERNAME")
-    #AUTH_USER = int(os.environ.get("AUTH_USER", 5071059420))
+    API_ID = int(os.environ.get("API_ID", "28356026"))
+    API_HASH = os.environ.get("API_HASH", "5fea2d52ca31906c83cf6ca8fc8a4449")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "7040863690:AAHKLfJZ5EnbA5YfbgEKmjROZtYK_dFewh8")
+    BOT_USERNAME = os.environ.get("BOT_USERNAME", "Myprsnabot")
+    #AUTH_USER = int(os.environ.get("AUTH_USER", 6790808572))
 
 
 Config = ENV_VARS
